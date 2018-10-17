23ef8ff7d5af4853ae7d8163474aca0b36b117f8
vnikolov4 <43233694+vnikolov4@users.noreply.github.com>
2018-10-18 01:14:09 +0300

    Update Win_Conf.ps1

--- Win_Conf.ps1
+++ Win_Conf.ps1
@@ -1,4 +1,4 @@
-﻿Import-Module International
- Set-WinSystemLocale -SystemLocale "en-NZ"
+Import-Module International
+Set-WinSystemLocale -SystemLocale "en-NZ"
 Set-Culture "en-NZ"
 (Get-WmiObject Win32_NetworkAdapterConfiguration -filter "ipenabled = 'true'").SetTcpipNetbios(2)
